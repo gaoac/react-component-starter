cf42b5c733b10a8bf232b55b796b513f5391e797
gaoac <gaoac.snow@outlook.com>
2020-01-14 17:01:27 +0800

    feat: 支持React,优化webpack多环境配置

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,3 +1,20 @@
 const path = require("path");
 
-module.exports = {};
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ]
+  }
+};
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,3 +1,31 @@
 const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.config.common.js");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {};
+module.exports = merge(common, {
+  mode: "development",
+  entry: {
+    app: "./example/src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist")
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    open: true,
+    port: 9000,
+    compress: true,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'example/src/index.html',
+      inject: 'body',
+      hash: false
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
