c8a531802cfd6f921077d7c953030434f9d36407
gaoac <gaoac.snow@outlook.com>
2020-01-16 09:44:46 +0800

    fix: 修复postcss版本过高报错的bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
-    require("autoprefixer")({ browsers: ["last 5 version", ">1%", "ie >=8"] })
+    require("autoprefixer")({
+      overrideBrowserslist: ["last 5 version", ">1%", "ie 11"]
+    })
   ]
 };
